acca34f5e6d3bdf8013be36e5abd58d47420b96a
M Dwyer <mark.andrew.dwyer@gmail.com>
2015-09-26 10:35:05 +1000

    Add install script for chocolatey apps

---
+++ scripts/windows/installtools.ps1
@@ -0,0 +1,18 @@
+﻿iex ((New-Object Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+cinst sysinternals -y
+cinst dependencywalker -y
+cinst windbg -y
+
+# For some reason, AutoHotkey.portable wasn't working with WinPcap, so use the full installation
+cinst autohotkey -y
+$env:Path = "$($env:Path)C:\Program Files\AutoHotkey;" 
+
+cinst winpcap -y
+cinst wireshark -y -i
+cinst 7zip -y
+cinst putty -y
+cinst fiddler -y
+cinst processhacker -y
+cinst apm -y
+cinst regshot -y
