88f88937d900b26d14bc598f99b5498c215a974d
hugo.genesse <hugo.genesse@polymtl.ca>
2016-04-06 15:13:37 -0400

    HG: Windows 8.1x64 testing machine. Not yet tested.

---
+++ vagrantfiles/windows-81x64.rb
@@ -0,0 +1,16 @@
+Vagrant.configure(2) do |config|
+	config.vm.box = "win81x64"
+	config.vm.guest = :windows
+	config.vm.boot_timeout = 1
+
+	config.vm.provider "virtualbox" do |vb|
+		vb.name = "win81x64"
+		vb.gui = true
+		vb.customize ["modifyvm", :id, "--nic1", "intnet"]
+		vb.customize ["modifyvm", :id, "--intnet1", "malwarelab"]
+		vb.customize ["modifyvm", :id, "--vram", "32"]
+		vb.customize ["modifyvm", :id, "--draganddrop", "hosttoguest"]
+		#config.vm.provision "shell",
+		#  inline: "New-PSDrive -Name \"V\" -PSProvider FileSystem -Root \"\\\\VBOXSVR\\vagrant\" -Persist"
+	end
+end
