GoSecure/malboxes

commit 68a32dda85ef59a6e43e9f85d612613786084078

Packer Templates now built using Jinja2 template engine
* Allows for logic to be kept outside of Python code making
  customization and contributions easier
* Ported profile tests to Python and doing a Jinja test and a json test

malboxes/malboxes.py
@@ -163,35 +163,30 @@ def prepare_packer_template(config, template_name):
     Prepares a packer template JSON file according to configuration and writes
     it into a temporary location where packer later expects it.
 
-    We need to do this since we are composing several JSON snippets based on
-    features enabled in configuration.
+    Uses jinja2 template syntax to generate the resulting JSON file.
+    Templates are in profiles/ and snippets in profiles/snippets/.
     """
-    template_fd = resource_stream(__name__,
-                                 'profiles/{}.json'.format(template_name))
-    template = json.load(TextIOWrapper(template_fd))
+    try:
+        profile_fd = resource_stream(__name__,
+                                     'profiles/{}.json'.format(template_name))
+    except FileNotFoundError:
+        print("Profile doesn't exist: {}".format(template_name))
+        sys.exit(2)
 
-    # merge optional configurations
-    # IDA remote debugger, based on target architecture
-    if config.get('ida_path'):
-        if _is_os_32bits(config):
-            template['provisioners'].append(fetch_snippet('ida_remote_32'))
-        else:
-            template['provisioners'].append(fetch_snippet('ida_remote_64'))
-
-    # tools_path for tools upload
-    if config.get('tools_path'):
-        template['provisioners'].append(fetch_snippet('tools'))
+    filepath = resource_filename(__name__, 'profiles/')
+    env = Environment(loader=FileSystemLoader(filepath), autoescape=False)
+    template = env.get_template("{}.json".format(template_name))
 
     # write to temporary file
     f = create_cachefd('{}.json'.format(template_name))
-    f.write(json.dumps(template))
+    f.write(template.render(config)) # pylint: disable=no-member
     f.close()
     return f.name
 
 
-def load_config(profile):
+def prepare_config(profile):
     """
-    Load Malboxes configuration and merge with Packer profile configuration
+    Prepares Malboxes configuration and merge with Packer profile configuration
 
     Packer uses a configuration in JSON so we decided to go with JSON as well.
     However since we have features that should be easily "toggled" by our users
@@ -204,13 +199,6 @@ def load_config(profile):
 
     [1]: https://plus.google.com/+DouglasCrockfordEsq/posts/RK8qyGVaGSr
     """
-    try:
-        profile_fd = resource_stream(__name__,
-                                     'profiles/{}.json'.format(profile))
-    except FileNotFoundError:
-        print("Profile doesn't exist: {}".format(profile))
-        sys.exit(2)
-
     # if config does not exist, copy default one
     config_file = os.path.join(DIRS.user_config_dir, 'config.js')
     if not os.path.isfile(config_file):
@@ -219,32 +207,34 @@ def load_config(profile):
         shutil.copy(resource_filename(__name__, 'config-example.js'),
                     config_file)
 
-    # load general config
+    config = load_config(config_file, profile)
+
+    packer_tmpl = prepare_packer_template(config, profile)
+
+    # merge/update with profile config
+    with open(packer_tmpl, 'r') as f:
+        config.update(json.loads(f.read()))
+
+    return config, packer_tmpl
+
+
+def load_config(config_file, profile):
+    """Loads the minified JSON config. Returns a dict."""
     config = {}
     with open(config_file, 'r') as f:
         # minify then load as JSON
         config = json.loads(jsmin(f.read()))
 
-    # merge/update with profile config
-    config.update(json.load(TextIOWrapper(profile_fd)))
-
+    # add packer required variables
+    config['cache_dir'] = DIRS.user_cache_dir
+    config['dir'] = resource_filename(__name__, "")
+    config['profile_name'] = profile
     return config
-
-
-def fetch_snippet(filename):
-    """Returns given snippet filename parsed as json"""
-    snippet = resource_stream(__name__,
-                              'profiles/snippets/{}.json'.format(filename))
-    return json.load(TextIOWrapper(snippet))
 
 
 def _get_os_type(config):
     """OS Type is extracted from profile json config"""
     return config['builders'][0]['guest_os_type'].lower()
-
-
-def _is_os_32bits(config):
-    return not _get_os_type(config)[-3:] == '_64'
 
 
 tempfiles = []
@@ -302,10 +292,7 @@ def run_packer(packer_tmpl, args):
             f.write(jsmin(config.read()))
             f.close()
 
-        filepath = resource_filename(__name__, "")
-        flags = ['-var-file={}'.format(f.name),
-                 "-var", "malboxes_cache_dir={}".format(DIRS.user_cache_dir),
-                 "-var", "malboxes_dir={}".format(filepath)]
+        flags = ['-var-file={}'.format(f.name)]
         if args.debug:
             flags.append('-debug')
 
@@ -356,14 +343,14 @@ def list_profiles(parser, args):
 
 
 def build(parser, args):
-    config = load_config(args.profile)
 
     print("Generating configuration files...")
+    config, packer_tmpl = prepare_config(args.profile)
     prepare_autounattend(config)
-    filename = prepare_packer_template(config, args.profile)
     print("Configuration files are ready")
+
     if not args.skip_packer_build:
-        ret = run_packer(filename, args)
+        ret = run_packer(packer_tmpl, args)
     else:
         ret = 0
 
@@ -401,7 +388,7 @@ def spin(parser, args):
     """
     Creates a Vagrantfile based on a template using the jinja2 engine
     """
-    config = load_config(args.profile)
+    config = prepare_config(args.profile)
 
     print("Creating a Vagrantfile")
     filepath = resource_filename(__name__, "vagrantfiles/")

Makefile
@@ -1,7 +1,7 @@
 test:
 	pylint malboxes
-	./tests/profiles_json_valid.sh
 	./tests/config_example_valid.sh
+	./tests/profiles_json_valid.py
 
 pkg_clean:
 	rm -r build/ dist/ malboxes.egg-info/

tests/profiles_json_valid.py
@@ -1,0 +1,56 @@
+#!/usr/bin/env python3
+#
+# This file is part of the Malboxes project.
+#
+# Malboxes - Vagrant box builder and config generator for malware analysis
+# https://github.com/gosecure/malboxes
+#
+# Olivier Bilodeau <obilodeau@gosecure.ca>
+# Copyright (C) 2016 GoSecure Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+import glob
+import json
+import os
+import re
+import sys
+
+from jinja2 import Environment, FileSystemLoader
+
+from malboxes.malboxes import load_config
+
+env = Environment(loader=FileSystemLoader('malboxes/profiles/'),
+                  autoescape=False)
+for profile in glob.glob("malboxes/profiles/*.json"):
+    print("Processing file {}".format(profile))
+
+    # process profile
+    profile_name = os.path.basename(profile)
+    config = load_config('config-example.js', 
+                         re.match('(.*).json$', profile_name).group(1))
+
+    try:
+        template = env.get_template(os.path.basename(profile_name))
+        profile_json = template.render(config) # pylint: disable=no-member
+        print("Properly formatted Jinja2 template")
+    except:
+        print("Badly formatted Jinja2 template! Failing test...")
+        sys.exit(1)
+
+    # test if json is valid
+    try:
+        json.loads(profile_json)
+        print("Properly formatted JSON file")
+    except:
+        print("Badly formatted JSON file! Failing test...")
+        sys.exit(2)

tests/profiles_json_valid.sh
@@ -1,14 +1,0 @@
-#!/bin/bash
-
-DIRS="malboxes/profiles/*.json malboxes/profiles/snippets/*.json"
-
-for F in `ls $DIRS`; do
-	echo "Processing file $F";
-	python3 -m json.tool $F 1>/dev/null
-	if [[ $? -ne 0 ]]; then
-		echo "Badly formatted JSON file! Failing test...";
-		exit 1;
-	else
-		echo "Properly formatted JSON file";
-	fi
-done

commit 68ed93693620da6c5ec1313a1e202c120493291f

Fixup for bug in `spin` introduced in 68a32dda8

malboxes/malboxes.py
@@ -394,7 +394,7 @@ def spin(parser, args):
     """
     Creates a Vagrantfile based on a template using the jinja2 engine
     """
-    config = prepare_config(args.profile)
+    config, _ = prepare_config(args.profile)
 
     print("Creating a Vagrantfile")
     filepath = resource_filename(__name__, "vagrantfiles/")