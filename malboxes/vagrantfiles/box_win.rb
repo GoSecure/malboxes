0acf4036e45e14364c8b5354bdcba3adf57a4f9a
Olivier Bilodeau <obilodeau@gosecure.ca>
2016-08-08 14:33:39 -0400

    Fix: WinRM user/pass regression in vagrant introduced in e486efb

--- malboxes/vagrantfiles/box_win.rb
+++ malboxes/vagrantfiles/box_win.rb
@@ -1,6 +1,8 @@
 Vagrant.configure(2) do |config|
 	config.vm.guest = :windows
 	config.vm.communicator = :winrm
+	config.winrm.username = "{{ username }}"
+	config.winrm.password = "{{ password }}"
 
 	# Giving plenty of times for updates
 	config.vm.boot_timeout = 600
