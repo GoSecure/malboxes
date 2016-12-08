5e3c8b8b872a6bb5511bb3d5cc746bc4e04392fe
Olivier Bilodeau <obilodeau@gosecure.ca>
2016-12-08 14:39:29 -0500

    Fix travis build. Regression caused in 3f2e9d8

--- tests/config_example_valid.sh
+++ tests/config_example_valid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-F=config-example.js
+F=malboxes/config-example.js
 echo "Processing file $F";
 # minifying first to strip comments
 python3 -m jsmin $F | python3 -m json.tool 1>/dev/null
