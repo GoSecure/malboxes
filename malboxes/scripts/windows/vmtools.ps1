e6ea4995ab9027d1c80f7fad1a1fa3877330c1a8
Olivier Bilodeau <obilodeau@gosecure.ca>
2017-01-09 11:27:02 -0500

    VirtualBox Guest Additions: Support for the new certificate name (fixes #24)

--- malboxes/scripts/windows/vmtools.ps1
+++ malboxes/scripts/windows/vmtools.ps1
@@ -2,5 +2,12 @@ $vboxAdditionsDrive = Get-WmiObject Win32_LogicalDisk -Filter "DriveType=5" |
 			Where-Object { $_.VolumeName -Like '*VBOX*' } |
 			Select -ExpandProperty DeviceID
 
-&certutil -addstore -f "TrustedPublisher" "$vboxAdditionsDrive\cert\oracle-vbox.cer"
+# Starting with VirtualBox 5.1.12 the name of the certificate changed
+$newCertName = "vbox-sha1.cer"
+if (Test-Path "$vboxAdditionsDrive\cert\$newCertName") {
+    &certutil -addstore -f "TrustedPublisher" "$vboxAdditionsDrive\cert\$newCertName"
+}
+else {
+    &certutil -addstore -f "TrustedPublisher" "$vboxAdditionsDrive\cert\oracle-vbox.cer"
+}
 &"$vboxAdditionsDrive\VBoxWindowsAdditions.exe" /S
