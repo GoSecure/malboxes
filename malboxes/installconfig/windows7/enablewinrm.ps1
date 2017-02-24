5682c9962ecf089ba86aef510312edcdcec17529
Olivier Bilodeau <obilodeau@gosecure.ca>
2017-02-23 23:02:49 -0500

    Trying new code to set the network location (#33)
    Since Get-NetConnectionProfile and Set-NetConnectionProfile doesn't
    exist on Windows 7

--- malboxes/installconfig/windows7/enablewinrm.ps1
+++ malboxes/installconfig/windows7/enablewinrm.ps1
@@ -1,4 +1,25 @@
-Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+// ChangeCategory.ps1
+//
+$NLMType = [Type]::GetTypeFromCLSID('DCB00C01-570F-4A9B-8D69-199FDBA5723B')
+$INetworkListManager = [Activator]::CreateInstance($NLMType)
+
+$NLM_ENUM_NETWORK_CONNECTED  = 1
+$NLM_NETWORK_CATEGORY_PUBLIC = 0x00
+$NLM_NETWORK_CATEGORY_PRIVATE = 0x01
+$UNIDENTIFIED = "Unidentified network"
+
+$INetworks = $INetworkListManager.GetNetworks($NLM_ENUM_NETWORK_CONNECTED)
+
+foreach ($INetwork in $INetworks)
+{
+    $Name = $INetwork.GetName()
+    $Category = $INetwork.GetCategory()
+
+    if ($INetwork.IsConnected -and ($Category -eq $NLM_NETWORK_CATEGORY_PUBLIC) -and ($Name -eq $UNIDENTIFIED))
+    {
+        $INetwork.SetCategory($NLM_NETWORK_CATEGORY_PRIVATE)
+    }
+}
 
 winrm quickconfig -q -transport:http
 winrm set winrm/config/client '@{AllowUnencrypted="true"}'
