5c17ae22bf97ad52b4e7b22f859f3bd4402c19e0
Olivier Bilodeau <obilodeau@gosecure.ca>
2017-02-23 23:04:24 -0500

    Windows7: sc.exe arguments needs a space after the =

--- malboxes/installconfig/windows7/enablewinrm.ps1
+++ malboxes/installconfig/windows7/enablewinrm.ps1
@@ -28,5 +28,5 @@ winrm set winrm/config/service/auth '@{Basic="true"}'
 net stop winrm
 netsh advfirewall firewall set rule group="remote administration" new enable=yes
 netsh firewall add portopening TCP 5985 "Port 5985"
-sc.exe config winrm start=auto
+sc.exe config winrm start= auto
 net start winrm
