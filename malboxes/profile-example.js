c443c0e05f7c67a441a56af6dbee96115150918d
Hugo Genesse <hugo@pnfsoftware.com>
2017-07-21 00:44:41 -0400

    Added the default profile.

---
+++ malboxes/profile-example.js
@@ -0,0 +1,20 @@
+{
+
+    "package": [{"package": "thunderbird"}],
+    "document": [{"modtype": "add", "docpath": "C:\\Malboxes"}],
+    "directory": [{"modtype": "add", "dirpath": "C:\\mlbxs\\"}],
+    "registry": [
+        {"modtype": "add", "key": "HKLM:\\Hardware\\Description\\System", "value": "04/04/04", "name": "SystemBiosDate", "valuetype": "String"},
+        {"modtype": "add", "key": "HKLM:\\Hardware\\Description\\System", "value": "Hardware Version 0.0 PARTTBLX", "name": "SystemBiosDate", "valuetype": "String"},
+        {"modtype": "delete", "key": "HKLM:\\HARDWARE\\ACPI\\DSDT\\VBOX__"},
+        {"modtype": "delete", "key": "HKLM:\\HARDWARE\\ACPI\\FADT\\VBOX__"},
+        {"modtype": "delete", "key": "HKLM:\\HARDWARE\\ACPI\\RSDT\\VBOX__"},
+        {"modtype": "delete", "key": "HKLM:\\SYSTEM\\ControlSet001\\Services\\VBoxGuest"},
+        {"modtype": "delete", "key": "HKLM:\\SYSTEM\\ControlSet001\\Services\\VBoxMouse"},
+        {"modtype": "delete", "key": "HKLM:\\SYSTEM\\ControlSet001\\Services\\VBoxService"},
+        {"modtype": "delete", "key": "HKLM:\\SYSTEM\\ControlSet001\\Services\\VBoxSF"},
+        {"modtype": "delete", "key": "HKLM:\\SYSTEM\\ControlSet001\\Services\\VBoxVideo"},
+        {"modtype": "add", "key": "HKLM:\\HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", "value": "Malboxes", "name": "Identifier", "valuetype": "String"}]
+
+
+}
