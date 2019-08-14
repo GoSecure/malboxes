fa71f3367aa6217926c12041c2a4fdea440035ee
Olivier Bilodeau <obilodeau@gosecure.net>
2019-08-14 17:26:18 -0400

    Added boto3 requirement to setup.py

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['appdirs', 'Jinja2', 'jsmin'],
+    install_requires=['appdirs', 'Jinja2', 'jsmin', 'boto3'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
