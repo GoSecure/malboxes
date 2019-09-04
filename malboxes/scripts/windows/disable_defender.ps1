85f57392d2cd28e7d8a2e2b9771785517cd728e9
Olivier Bilodeau <obilodeau@gosecure.net>
2019-09-04 13:39:30 -0400

    Revert "Found a way to disable the new Defender Tamper Protection"
    This reverts commit 6abefd4f8823b59f63bf4271433d14ddd653ff59.

--- malboxes/scripts/windows/disable_defender.ps1
+++ malboxes/scripts/windows/disable_defender.ps1
@@ -1,8 +1,3 @@
-
-# This disables Tamper Protection introduced in Windows 10 1903 (May 2019 Update)
-# but it requires us to run disable_defender_tamper_protection.ps1 beforehand to allow registry changes
-Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows Defender\Features" -Name "TamperProtection" -Value 0
-
 # Disable Windows Defender on Windows 10
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" -Name "DisableRoutinelyTakingAction" -Value 1
